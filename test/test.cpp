6b7e1d77b37a04fd5776f53d77978a2c8090f0a1
koustubh1012 <koustubh1012@gmail.com>
2024-10-07 11:41:41 -0400

    Task #5 Added Unit tests
    In this commit we have added the unit tests for the project

--- test/test.cpp
+++ test/test.cpp
@@ -1,15 +1,71 @@
+#include "pid_controller.hpp"
 #include <gtest/gtest.h>
-#include "lib1.hpp"
-#include "lib2.hpp"
+#include <vector>
 
-TEST(dummy_test, this_should_pass) {
-  EXPECT_EQ(1, 1);
+//Test to check if the robot does not move if it is already at target 
+TEST(PIDControllerTest, NoMovementTest_1){
+    // Create object of PIDController class
+    PIDController controller(0.0, 0.0, 0.1, 0.1);
+    std::vector<double> robot_state;
+    //set the target for the robot
+    controller.set_target(0, 0);
+    //tune the PID parameters
+    controller.tune_PID(1, 0, 0.1);
+    // move the robot and return the final x and y coordinates
+    robot_state = controller.move_robot();
+
+    // Test if the x and y positions are correct
+    EXPECT_NEAR(robot_state[0], 0, 0.1);
+    EXPECT_NEAR(robot_state[1], 0, 0.1);
+}
+    // Create object of PIDController class
+//Test to check if the robot does not move if it is already at target 
+TEST(PIDControllerTest, NoMovementTest_2){
+
+    PIDController controller(3.0, 4.0, 0.1, 0.1);
+    std::vector<double> robot_state;
+    //set the target for the robot
+    controller.set_target(3, 4);
+    //tune the PID parameters
+    controller.tune_PID(1, 0, 0.1);
+    // move the robot and return the final x and y coordinates
+    robot_state = controller.move_robot();
+
+    // Test if the x and y positions are correct
+    EXPECT_NEAR(robot_state[0], 3, 0.1);
+    EXPECT_NEAR(robot_state[1], 4, 0.1);
 }
 
-TEST(dummy_test, this_should_pass_too) {
-  EXPECT_EQ(my_function1(3), 3);
+// Test to check if the PID controller can move the robot to the target position
+TEST(PIDControllerTest, MovementTest_1){
+    // Create object of PIDController class
+    PIDController controller(0.0, 0.0, 0.1, 0.1);
+    std::vector<double> robot_state;
+    //set the target for the robot
+    controller.set_target(5, 6);
+    //tune the PID parameters
+    controller.tune_PID(1, 0, 0.1);
+    // move the robot and return the final x and y coordinates
+    robot_state = controller.move_robot();
+
+    // Test if the x and y positions are correct
+    EXPECT_NEAR(robot_state[0], 5, 0.1);
+    EXPECT_NEAR(robot_state[1], 6, 0.1);
 }
 
-TEST(dummy_test, this_will_fail) {
-  EXPECT_EQ(my_function2(3.2), 3.2);
+// Test to check if the PID controller can move the robot to the target position
+TEST(PIDControllerTest, MovementTest_2){
+    // Create object of PIDController class
+    PIDController controller(2.0, 3.0, 0.1, 0.1);
+    std::vector<double> robot_state;
+    //set the target for the robot
+    controller.set_target(20, 30);
+    //tune the PID parameters
+    controller.tune_PID(1, 0, 0.1);
+    // move the robot and return the final x and y coordinates
+    robot_state = controller.move_robot();
+
+    // Test if the x and y positions are correct
+    EXPECT_NEAR(robot_state[0], 20, 0.1);
+    EXPECT_NEAR(robot_state[1], 30, 0.1);
 }
